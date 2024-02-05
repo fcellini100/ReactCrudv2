aba50349cc09a2829d91923f13918f520283ce1a
Fernando Cellini <fernandocellini100@gmail.com>
2024-02-05 15:11:52 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl text-blue-950 font-bold bg-gray-300">
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tests/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../App';
+
+describe('App component', () => {
+  test('renders "Hello world" text', () => {
+    render(<App />);
+    const helloWorldElement = screen.getByText(/hello world/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    expect(helloWorldElement).toHaveClass(
+      'text-3xl text-blue-950 font-bold bg-gray-300'
+    );
+  });
+});
