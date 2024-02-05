6ca0d62f20a8b02b18a134cefb57b62fa9035a60
Fernando Cellini <fernandocellini100@gmail.com>
2024-02-05 16:25:36 +0000

    added router config and dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-function App() {
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Dashboard } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+]);
+
+const App = () => {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-3xl text-blue-950 font-bold bg-gray-300">
-        Hello world
-      </h1>
-    </div>
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className="text-3xl text-blue-950 font-bold bg-gray-300">
+          Hello world
+        </h1>
+      </div>
+      <RouterProvider router={router} />
+    </>
   );
-}
+};
 
 export default App;
