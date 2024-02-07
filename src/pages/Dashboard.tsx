b488ea6db3c1780cfb613c23551caa1a22616e7f
Fernando Cellini <fernandocellini100@gmail.com>
2024-02-07 16:13:27 +0000

    added something to look at in dashboard + pending use context to store user data

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,35 @@
+import { useFetchUserData } from '@/hooks';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Spinner,
+} from '@nextui-org/react';
+import { User as UserIcon } from 'lucide-react';
+
 export const Dashboard = () => {
-  return <h1>Hello Component works!</h1>;
+  const { data: users, isLoading } = useFetchUserData();
+
+  const userCount = users ? users.length : 0;
+  return (
+    <div className="container m-auto my-8">
+      <h1 className="text-3xl font-semibold">Dasboard</h1>
+      <Divider />
+
+      <div className="m-auto my-4 flex gap-4 flex-wrap">
+        <Card className="py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
+            <p className="text-center">
+              <UserIcon />
+            </p>
+            <h4 className="font-semibold text-large">Total Users</h4>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2 text-center font-bold">
+            {isLoading ? <Spinner size="sm" /> : userCount}
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
 };
